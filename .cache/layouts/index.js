19a1836a2b052257dd95c8f5c116d1e057da9c06
scemii <pierre_raffalli@msn.com>
2018-11-28 23:20:37 +0100

    ajout comonent cards

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/pierre/Documents/GitHub/canonA1_2/src/layouts/index.js"
+  import data from "/Users/pierre/Documents/GitHub/canonA1_2/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
